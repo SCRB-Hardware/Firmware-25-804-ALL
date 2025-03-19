c33dbef80ea609b642deef963b1321b9ffae6543
Tony <tony0953112356@gmail.com>
2025-03-18 23:33:54 -0400

    update lib-limit-switch.c

--- Library/lib-limit-switch.c
+++ Library/lib-limit-switch.c
@@ -5,17 +5,18 @@
 /* ------ Limit switches ------ */
 
 void LMSW_Init(void) {
-	// PD0~3: Limit switches 1~4
-	GPIOD->MODER = GPIOD->MODER & 0xFFFFFF00; 
+	// Input mode for PD1 and PD2
+	GPIOD->MODER = GPIOD->MODER & 0xFFFFFFC3; 
 }
 
 int LMSW_Read(int index) {
 	// Returns whether the switch is closed (active)
-	if(index < 1 || index > 4) return 0; 
+	// *********Need to verify in dss_home()***************Not sure about it
+	if(index != 2 || index != 3) return 0; 
 	return !(GPIOD->IDR & (1 << (index - 1))); 
 }
 
 int LMSW_ReadAll(void) {
 	// Returns all switches as a bitmap
-	return ~GPIOD->IDR & 0xF; 
+	return ~GPIOD->IDR & 0x6; 
 }
